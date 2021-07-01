acb7be7d3022a1622ce4d54cccf6b0307369ce3e
ivandiazperez <42877828+ivandiazperez@users.noreply.github.com>
2021-07-01 09:43:00 +0200

    Update delegatetovali.sh

--- delegatetovali.sh
+++ delegatetovali.sh
@@ -6,4 +6,4 @@ FEE=5000
 #Put your wallet pubkey in the next line:
 AMOUNT=$(desmos query bank balances yourwalletpubkey --chain-id morpheus-apollo-1 --output=json | jq -r '.balances[].amount')
 DIFFACC=$(($AMOUNT-1000000))
-desmos tx staking delegate ${VALOPER} ${DIFFAC}udaric --from ${WALLET} --chain-id ${CHAIN_ID} --fees ${FEE}udaric -y
+desmos tx staking delegate ${VALOPER} ${DIFFACC}udaric --from ${WALLET} --chain-id ${CHAIN_ID} --fees ${FEE}udaric -y
